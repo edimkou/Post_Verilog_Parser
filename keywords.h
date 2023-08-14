27fe291d4190f5841c86d2532146e565d9d0dfee
Eleni Dimkou <77272704+edimkou@users.noreply.github.com>
2023-08-14 16:08:03 +0300

    Update keywords.h

--- keywords.h
+++ keywords.h
@@ -7,28 +7,15 @@
 ** keywords.h
 ** Verilog keywords
 **
-** Author: David Kebo Houngninou
+** Author: Dimkou Eleni
 ** -------------------------------------------------------------------------*/
 #ifndef _keywords_h
 #define _keywords_h
-
-#ifdef IDENT_H
-static const char *const keywords_h_Id =
-    "$Id$";
-#endif
 
 #define GATESNUM 20
 extern const char *GATE_NAME[GATESNUM];
 
 #define RESERVEDNUM 34
 extern const char *RESERVED_WORD[RESERVEDNUM];
-
-// #define NUM_PUNCTUATIONS 4
-// char *punctuations[] = {
-//     ";",
-//     ".",
-//     " ",
-//     "\n",
-// };
 
 #endif
